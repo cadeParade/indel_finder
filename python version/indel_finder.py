d4d3a6826d212468d880d9e82e59a36156b2febb
Lindsay Cade <lrc3233@gmail.com>
2013-03-20 09:58:10 -0700

    .

--- python version/indel_finder.py
+++ python version/indel_finder.py
@@ -58,65 +58,13 @@ def annotate_sequence(sequence, bpcutoff, npctcutoff):
 		sequence.initial_screen = "OK"	
 	
 	
-# def annotate_sequences(sequence_list, bpcutoff, npctcutoff):
-# 	
-# 	sequence_dict = {}
-# 	first_binding_site_indexes = []
-# 	second_binding_site_indexes = []
-# 	first_binding_site_indexes_rev = []
-# 	second_binding_site_indexes_rev = []
-# 		
-# 	for i,sequence in enumerate(sequence_list):
-	
-		# #assign well number to name field
-# 		current_well_no = re.split("([A-H])(\\d\\d)", sequence.name)
-# 		current_well_no = "".join(current_well_no[1:3])
-# 		sequence.name = current_well_no
-# 		
-# 		#assign length and n count
-# 		sequence.length = len(sequence)
-# 		sequence.n_count = sequence.seq.count("N")
-# 		
-# 		#find percent "N" of sequence
-# 		sequence.n_pct = (Decimal(sequence.n_count)/
-# 						  Decimal(sequence.length)) * 100
-# 		
-# 		#screens for too few bps or too many N's
-# 		if (sequence.n_pct > npctcutoff and 
-# 			sequence.length < bpcutoff):
-# 			sequence.initial_screen = ("sequence too short" + 
-# 									   "and too many N's")
-# 		elif sequence.n_pct > npctcutoff:
-# 			sequence.initial_screen = "too many N's"
-# 		elif sequence.length < bpcutoff:
-# 			sequence.initial_screen = "sequence too short"
-# 		else:
-# 			sequence.initial_screen = "OK"	
-# 		
-#		first_binding_site_indexes.append(
-#				find_best_match_location(sequence, bind1))
-#		first_binding_site_indexes_rev.append(
-	# 				find_best_match_location(
-# 					sequence.reverse_complement(), bind1))
-# 		print sequence.name
-# 		print first_binding_site_indexes[i][2], "forward"
-# 		print first_binding_site_indexes_rev[i][2], "reverse"
-# 		
-# 	
-	# print sequence_list[1].name, "name"
-# 	print sequence_list[1].length, "length"
-# 	print sequence_list[1].n_count, "n count"
-# 	print sequence_list[1].n_pct, "n pct"
-# 	print sequence_list[1].initial_screen, "initial screen"
-# 	print sequence_list[1]
-	
 def decide_sequence_direction(sequence, fwd_tuple, rev_tuple):
 	if fwd_tuple[2] < rev_tuple[2] and fwd_tuple[2] < 3:
 		sequence.direction = "forward"
-		sequence.bind1 = fwd_tuple
+		sequence.bind1 = (fwd_tuple[0], fwd_tuple[1])
 	elif fwd_tuple[2] > rev_tuple[2] and rev_tuple[2] < 3:
 		sequence.direction = "reverse"
-		sequence.bind1 = rev_tuple
+		sequence.bind1 = (rev_tuple[0], rev_tuple[1])
 	else:
 		sequence.direction = "dunno"
 	print sequence.name
