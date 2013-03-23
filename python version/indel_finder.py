68968bdb913a52835378bcd9d6c33d6214a61963
Lindsay Cade <lrc3233@gmail.com>
2013-03-22 21:09:01 -0700

    added csv creation functionality to display summary of results
    * cleaned up extranneous comments
    * made things more concise

--- python version/indel_finder.py
+++ python version/indel_finder.py
@@ -4,14 +4,15 @@ import numpy as np
 import string
 import re
 from decimal import *
+import csv
 
 #nrxn
-bind1 = "ATCTTCAGC" 
-bind2 = "GATGAGGTT"
+#bind1 = "ATCTTCAGC" 
+#bind2 = "GATGAGGTT"
 
 #epas
-# bind1 = "TACAATACTCCCACTGAA"
-# bind2 = "ACTCATGGACAGTTGGTA"
+bind1 = "TACAATACTCCCACTGAA"
+bind2 = "ACTCATGGACAGTTGGTA"
 # target = "ATGACAGATGCAGACAG"
 #wt = "cactgttgttaggagggttcagagtagcaggatgaagttgctgttgtttattttggatgtgagccaagggcttgagagctagaacaagactagtatagtgtgcacacacactaacttgcattctaaaactcttgtgtttgtgctgtattgcagGCTACAATACTCCCACTGAAATGACAGATGCAGACAGACTCATGGACAGTTGGTATCTGAAGTCACTCGGTGGCTTTATTACAGTGGTAACATCAGATGGAGACATGATCTTCTTATCGGAGAACATCAACAAtagtaacgcacactgtatcaacacatgaatcga"
 
@@ -24,10 +25,10 @@ def read_fasta(file_to_open):
 	for record in SeqIO.parse(seq_file, "fasta", 
 							  alphabet = generic_dna):
 		sequence_list.append(record)
+		record.initial_screen = "Omitted" 
 		record.length = None 
 		record.n_count = None 
 		record.n_pct = None 
-		record.initial_screen = "Omitted" 
 		record.direction = None 
 		record.bind1dir = None 
 		record.bind2dir = None 
@@ -66,11 +67,7 @@ def annotate_sequence(sequence, bpcutoff, npctcutoff):
 def find_best_match_location(sequence, search_string):
 	
 	score_list = []
-	#print sequence
-	#print sequence.initial_screen
-	#print len(sequence), "length of sequence"
-	#print len(search_string), "length of search string"
-	#if sequence.initial_screen == "OK":
+
 	# Make list of scores for each window
 	for i in range(0,len(sequence)-len(search_string)):
 		current_string = sequence[i: i+len(search_string)]
@@ -81,8 +78,7 @@ def find_best_match_location(sequence, search_string):
 			else:
 				score +=1
 		score_list.append(score)
-# 	else:
-# 		score_list = [None, None, None]
+
 	if score_list:
 		min_score_index = score_list.index(min(score_list))
 	else:
@@ -90,47 +86,37 @@ def find_best_match_location(sequence, search_string):
 		score_list = [None]
 	
 	#returns tuple of search strings starting and ending index
-	# and match score
+	#and match score
 	return (min_score_index, 
 			min_score_index + len(search_string), 
 			min(score_list))
 	
-def decide_sequence_direction(sequence, 
-							  fwd_tuple1, rev_tuple1, 
-							  fwd_tuple2, rev_tuple2):
+def decide_sequence_direction_for_one_only(sequence,
+ 										   fwd_tuple,
+ 										   rev_tuple):
 
 	#define when to rule out too many errors in binding site
 	acceptable_pct_ns_in_binding_site = len(bind1)* 0.25
-	
-	sequence_direction_list = []
-	
-	#assigns bind1 direction based on score for scanning
-	#binding site 1 forward and reverse
-	if (fwd_tuple1[2] < rev_tuple1[2] and 
-		fwd_tuple1[2] <= acceptable_pct_ns_in_binding_site 
+
+ 	if (fwd_tuple[2] < rev_tuple[2] and 
+		fwd_tuple[2] <= acceptable_pct_ns_in_binding_site 
 		and sequence.initial_screen == "OK"):	
-		sequence.bind1dir = "forward"
-	elif (fwd_tuple1[2] > rev_tuple1[2] and 
-		  rev_tuple1[2] <= acceptable_pct_ns_in_binding_site 
+		return "forward"
+	elif (fwd_tuple[2] > rev_tuple[2] and 
+		  rev_tuple[2] <= acceptable_pct_ns_in_binding_site 
 		  and sequence.initial_screen == "OK"):
-		sequence.bind1dir = "reverse"
+		return "reverse"
 	else:
-		sequence.bind1dir = "dunno"
-		
+ 		return "dunno"   
+										
+def decide_sequence_direction(sequence, 
+							  fwd_tuple1, rev_tuple1, 
+							  fwd_tuple2, rev_tuple2):
 	
-	#assigns bind1 direction based on score for scanning
-	#binding site 1 forward and reverse	
-	if (fwd_tuple2[2] < rev_tuple2[2] and 
-		fwd_tuple2[2] <= acceptable_pct_ns_in_binding_site 
-		and sequence.initial_screen == "OK"):	
-		sequence.bind2dir = "forward"
-	elif (fwd_tuple2[2] > rev_tuple2[2] and 
-		  rev_tuple2[2] <= acceptable_pct_ns_in_binding_site 
-		  and sequence.initial_screen == "OK"):
-		sequence.bind2dir = "reverse"
-	else:
-		sequence.bind2dir = "dunno"
-		
+ 	sequence.bind1dir = decide_sequence_direction_for_one_only(
+ 		sequence, fwd_tuple1, rev_tuple1)
+ 	sequence.bind2dir = decide_sequence_direction_for_one_only(
+ 		sequence, fwd_tuple2, rev_tuple2)
 		
 	#checks if bind1 and bind2 direction match. 	
 	if sequence.bind1dir == sequence.bind2dir:
@@ -138,39 +124,37 @@ def decide_sequence_direction(sequence,
 	else:
 		sequence.direction = "conflicting directions"
 	
-		
-		
-	
 
 def find_distance_between_bind_sites(sequence, fwd1, rev1, fwd2, rev2):
 
 	if sequence.direction == "forward":
 		sequence.distance_between_sites = fwd2[0]-fwd1[1]
-		print sequence.name, sequence.distance_between_sites, "forward"
 	elif sequence.direction == "reverse":
 		sequence.distance_between_sites = rev2[0] - rev1[1]
-		print sequence.name, sequence.distance_between_sites, "reverse"
 	else:
-		print "This sequence didn't have a distance"
 		sequence.distance_between_sites = None
 	
+def make_csv(sequence_list):
 
-def main():
-	
-	#### SETUP ######
-	file_to_open = "nrxn1.seq"
-	#file_to_open = "epas1b.seq"
+	myfile = open("summary.csv", 'w+b')
+
+	wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
+	wr.writerow(["well no", "initial screen", "seq length", 
+				"pct n", "direction", "spacer length"])
+	for i,well in enumerate(sequence_list):
+
+		row_list = [well.name, well.initial_screen, well.length,
+					well.n_pct, well.direction, well.distance_between_sites]
+		wr.writerow(row_list)
+
+
+def main_for_loop(sequence_list):
+
 	bp_cutoff_value = 275
 	n_pct_cutoff_value = 32
 	#sets how many digits for numbers defined as Decimal(#)
 	getcontext().prec = 3
-	
-	#running	
-	sequence_list = read_fasta(file_to_open)
-	
-	#test_list = [1,5,10,25,27,28,31,36,39,45,55,56,57,62,64,96]
-	#test_list_indexes = [x-1 for x in test_list]
-	
+
 	for i, sequence in enumerate(sequence_list):
 		#adds basic properties like length, num ns, pct ns
 		annotate_sequence(sequence, 
@@ -181,12 +165,12 @@ def main():
 		#in forward and reverse directions
 		fwd1  = find_best_match_location(sequence, bind1)
 		rev1 = find_best_match_location(sequence.reverse_complement(),
-									   bind1)
+									    bind1)
 		#Finds location of best match for binding site 2
 		#in forward and reverse directions
 		fwd2  = find_best_match_location(sequence, bind2)
 		rev2 = find_best_match_location(sequence.reverse_complement(),
-									   bind2)
+									    bind2)
 		#compares directions for binding site 1 and 2
 		#assigns overall sequence direction
 		decide_sequence_direction(sequence, 
@@ -196,6 +180,15 @@ def main():
 		find_distance_between_bind_sites(sequence, 
 										 fwd1, rev1, 
 										 fwd2, rev2)
+
+
+def main():
 	
+	#### SETUP ######
+	#file_to_open = "nrxn1.seq"
+	file_to_open = "epas1b.seq"
+	sequence_list = read_fasta(file_to_open)
+	main_for_loop(sequence_list)
+	make_csv(sequence_list)
 	
 main()
